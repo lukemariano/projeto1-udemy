5998a81160b6fb5f9c714759db2b9acb8ddd04e1
Lucas Mariano <lucas.svmariano@gmail.com>
2022-08-21 11:49:57 -0300

    Migrações criadas, arquivos estáticos e media adicionados no urlpatterns e models adicionados no admin

--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
 
 
 class Recipe(models.Model):
@@ -17,3 +24,10 @@ class Recipe(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
